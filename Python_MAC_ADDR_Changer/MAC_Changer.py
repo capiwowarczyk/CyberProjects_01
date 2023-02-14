f3347617427c86646b78db423914c01220cbc98b
Carson Piwowarczyk <piwowarczykca@gmail.com>
2023-02-14 14:48:36 -0700

    Simple Python MAC address changer

--- Python_MAC_ADDR_Changer/MAC_Changer.py
+++ Python_MAC_ADDR_Changer/MAC_Changer.py
@@ -8,20 +8,19 @@ import optparse
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address") #these two lines are adding arguments to be preformed in the linux termenal
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error("[-] Plese specify a interface, use --help for more info")
+        parser.error("[-] Plese specify a interface, use --help for more info") #error checking
     elif not options.new_mac:
-        parser.error("[-] Plese specify a new MAC, use --help for more info")
+        parser.error("[-] Plese specify a new MAC, use --help for more info") #error checking
     return options
 
-def change_mac(interface, new_mac):
+def change_mac(interface, new_mac): #this function is running the commands nesscary in the termnal to change the MAC address
     print("[+] Changing MAC address for " + interface + " to: " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
-    # this is a more secure way becasue it devides the commands into a list
 
 options = get_arguments()
 change_mac(options.interface, options.new_mac)
