ad72ca10d6d69109e6116fb82ce4e0a31ab02da9
Carson Piwowarczyk <111904658+capiwowarczyk@users.noreply.github.com>
2026-07-21 15:06:37 -0600

    Update PythonEmailer.py

--- Python-Emailer/PythonEmailer.py
+++ Python-Emailer/PythonEmailer.py
@@ -6,9 +6,9 @@ from email.message import EmailMessage
 from appPass import password
 
 #define all the vairables required to send the email
-emailSender = "carsonautoemail@gmail.com"
-emailPassword = password 
-emailReceiver = "piwowarczykca@gmail.com" #input who ill reviece the email here
+emailSender = "TEST@email.com"
+emailPassword = ##### 
+emailReceiver = "TEST@email.com" #input who ill reviece the email here
 subject = "This is a test email"
 body = """ 
 When you check out my programs, I hope you enjoy them!
